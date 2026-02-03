d30f95d2ef190b4777f2ca770deb22feac420d49
Dariusz Okoński <darek200180@gmail.com>
2026-02-03 08:18:52 +0100

    update exercise 23

--- Program.cs
+++ Program.cs
@@ -24,18 +24,4 @@
 
 
 
-using SandBox.Exercise_23;
-
-Console.WriteLine("Exercise 23");
-var exercise23 = new Exercise_23();
-
-List<string> words = new List<string> { "bobcat", "wolverine", "grizzly" };
-var result = exercise23.ProcessAll(words);
-
-Console.WriteLine("ProcessAll: ");
-foreach (var item in result)
-{
-    Console.WriteLine("item: " + item);
-}
-
-Console.ReadLine();
+Console.WriteLine("Program");
